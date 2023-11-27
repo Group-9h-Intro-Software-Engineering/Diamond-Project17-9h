ff0cfd47cc227d75251a9f718fd21f11bd026438
gkravatsky <gkravatsky@ufl.edu>
2023-11-27 15:20:29 -0800

    Implemented text box for help button

--- client/src/views/Mentor/Dashboard/Dashboard.jsx
+++ client/src/views/Mentor/Dashboard/Dashboard.jsx
@@ -11,6 +11,7 @@ import { useNavigate } from 'react-router-dom';
 export default function Dashboard() {
   const [classrooms, setClassrooms] = useState([]);
   const [value] = useGlobalState('currUser');
+  const [isHelpVisible, setIsHelpVisible] = useState(false); // New state for help visibility
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -34,10 +35,26 @@ export default function Dashboard() {
     navigate(`/classroom/${classroomId}`);
   };
 
+  const handleHelpClick = () => {
+    // Toggle the visibility of the help text box
+    setIsHelpVisible(!isHelpVisible);
+  };
+
   return (
     <div className='container nav-padding'>
-      <NavBar />
-      <div id='main-header'>Welcome {value.name}</div>
+      <NavBar onHelpClick={handleHelpClick} />
+      <div id='main-header'>
+        Welcome {value.name}
+      </div>
+      {/* Render the help text box based on the state */}
+      <div id='help-textbox' className={isHelpVisible ? 'active' : ''}>
+        {/* Add the close button */}
+        <div id='close-button' onClick={() => setIsHelpVisible(false)}>X</div>
+        {/* Add your help text content here */}
+        <p>Welcome to CASMM! To get started, choose a classroom. {/* Your help text goes here */}</p>
+        {/* Embed an image */}
+      </div>
+      
       <MentorSubHeader title={'Your Classrooms'}></MentorSubHeader>
       <div id='classrooms-container'>
         <div id='dashboard-card-container'>
