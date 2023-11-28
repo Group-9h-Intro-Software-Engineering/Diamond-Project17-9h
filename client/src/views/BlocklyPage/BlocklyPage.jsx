393155fafc8e8408151747c580fa277a7da6c769
gkravatsky <gkravatsky@ufl.edu>
2023-11-27 19:06:50 -0800

    implementd help button for activity editor

--- client/src/views/BlocklyPage/BlocklyPage.jsx
+++ client/src/views/BlocklyPage/BlocklyPage.jsx
@@ -1,19 +1,16 @@
-import { message } from "antd"
-import React, { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import BlocklyCanvasPanel from "../../components/ActivityPanels/BlocklyCanvasPanel/BlocklyCanvasPanel"
-import NavBar from "../../components/NavBar/NavBar"
-import {
-  getAuthorizedWorkspaceToolbox,
-  getActivityToolbox,
-  getActivityToolboxAll,
-} from "../../Utils/requests"
-import { useGlobalState } from "../../Utils/userState"
+import { message } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import BlocklyCanvasPanel from '../../components/ActivityPanels/BlocklyCanvasPanel/BlocklyCanvasPanel';
+import NavBar from '../../components/NavBar/NavBar';
+import { getAuthorizedWorkspaceToolbox, getActivityToolbox, getActivityToolboxAll } from '../../Utils/requests';
+import { useGlobalState } from '../../Utils/userState';
 
 export default function BlocklyPage({ isSandbox }) {
-  const [value] = useGlobalState("currUser")
-  const [activity, setActivity] = useState({})
-  const navigate = useNavigate()
+  const [value] = useGlobalState('currUser');
+  const [activity, setActivity] = useState({});
+  const [isHelpVisible, setIsHelpVisible] = useState(false); // New state for help visibility
+  const navigate = useNavigate();
 
   useEffect(() => {
     const setup = async () => {
@@ -72,12 +69,30 @@ export default function BlocklyPage({ isSandbox }) {
     setup()
   }, [isSandbox, navigate, value.role])
 
+  const handleHelpClick = () => {
+    // Toggle the visibility of the help text box
+    setIsHelpVisible(!isHelpVisible);
+  };
+
   return (
-    <div className="container nav-padding">
-      <NavBar />
-      <div className="flex flex-row">
+    <div className='container nav-padding'>
+      {/* Pass the handleHelpClick function to the NavBar */}
+      <NavBar onHelpClick={handleHelpClick} />
+      {/* Render the help text box based on the state */}
+      <div id='help-textbox' className={isHelpVisible ? 'active' : ''}>
+        {/* Add the close button */}
+        <div id='close-button' onClick={() => setIsHelpVisible(false)}>
+          X
+        </div>
+        {/* Add your help text content here */}
+        <p>
+          This is the activity editor. Once you have created your activities, you can edit and save the changes here. To save your changes to the activity, click Save to template.{/* Your help text goes here */}
+        </p>
+        {/* Embed an image */}
+      </div>
+      <div className='flex flex-row'>
         <BlocklyCanvasPanel activity={activity} setActivity={setActivity} isSandbox={isSandbox} />
       </div>
     </div>
-  )
+  );
 }
