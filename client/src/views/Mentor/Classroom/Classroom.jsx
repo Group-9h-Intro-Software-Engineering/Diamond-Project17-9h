ccd858760f2736eb5d85756cf5ae678f12008619
gkravatsky <gkravatsky@ufl.edu>
2023-11-28 12:02:44 -0800

    added help dialogue for classroom

--- client/src/views/Mentor/Classroom/Classroom.jsx
+++ client/src/views/Mentor/Classroom/Classroom.jsx
@@ -1,13 +1,11 @@
-import { React, useEffect } from 'react';
-import { Tabs } from 'antd';
+import { React, useEffect, useState } from 'react';
+import { Tabs, Button } from 'antd';
 import './Classroom.less';
-
 import NavBar from '../../../components/NavBar/NavBar';
 import Roster from './Roster/Roster';
 import Home from './Home/Home';
 import SavedWorkSpaceTab from '../../../components/Tabs/SavedWorkspaceTab';
-import LessonEditor from '../../ContentCreator/LessonEditor/LessonEditor';
-import { useSearchParams, useParams } from 'react-router-dom';
+import { useSearchParams, useParams, useNavigate } from 'react-router-dom';
 
 const { TabPane } = Tabs;
 
@@ -17,19 +15,38 @@ export default function Classroom({
   setSelectedActivity,
 }) {
   const [searchParams, setSearchParams] = useSearchParams();
-
+  const [isHelpVisible, setIsHelpVisible] = useState(false); // New state for help visibility
   const { id } = useParams();
   const tab = searchParams.get('tab');
   const viewing = searchParams.get('viewing');
+  const navigate = useNavigate();
 
   useEffect(() => {
     sessionStorage.setItem('classroomId', id);
-
   }, [id]);
+
+  const handleHelpClick = () => {
+    // Toggle the visibility of the help text box
+    setIsHelpVisible(!isHelpVisible);
+  };
 
   return (
     <div className='container nav-padding'>
-      <NavBar isMentor={true} />
+      <NavBar isMentor={true} onHelpClick={handleHelpClick} />
+      {/* Render the help text box based on the state */}
+      <div id='help-textbox' className={isHelpVisible ? 'active' : ''}>
+        {/* Add the close button */}
+        <div id='close-button' onClick={() => setIsHelpVisible(false)}>
+          X
+        </div>
+        {/* Add your help text content here */}
+        <p>
+          Welcome to your classroom. To view learning standards, click change. To see the activities within a learning standard, select the learning standard, then click review and set as active learning standard.{' '}
+          {/* Your help text goes here */}
+        </p>
+        <p>To edit your activities, click the pencil icon, then edit student template.</p>
+        {/* Embed an image */}
+      </div>
       <Tabs
         defaultActiveKey={tab ? tab : 'home'}
         onChange={(key) => setSearchParams({ tab: key })}
@@ -52,16 +69,9 @@ export default function Classroom({
             classroomId={id}
           />
         </TabPane>
-
-        {/* Adding new tab to access lesson editor */}
-        <TabPane tab='Lesson Editor' key='lesson editor'>
-          {/* <LessonEditor
-          // Lesson Editor requires props and other stuff that we can get from classroom data object
-          // Must redirect to ccdashboard??
-
-          /> */}
-        </TabPane>
       </Tabs>
+      {/* Add the "Help" button outside the Tabs */}
+      <Button onClick={handleHelpClick}>Help</Button>
     </div>
   );
 }
